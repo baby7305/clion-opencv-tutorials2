db0fb680bf445df128994f3acd8102859a956457
baby7305 <songhaibei7305@163.com>
2019-09-20 08:45:23 +0800

    logPolar函数

---
+++ polar/logPolar_OpenCV3.cpp
@@ -0,0 +1,20 @@
+#include<opencv2/core/core.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include<opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+
+int main(int argc, char *argv[]) {
+    //读入
+    Mat src = imread("../lena.jpg", IMREAD_ANYCOLOR);
+    // 对数极坐标变换
+    Mat dst;
+    Point2f center(508, 503);
+    float M = 100;
+    logPolar(src, dst, center, M, WARP_FILL_OUTLIERS);
+    //显示对数极坐标变换的效果
+    imshow("log polar transform", dst);
+    imshow("origin", src);
+    waitKey(0);
+    return 0;
+}
